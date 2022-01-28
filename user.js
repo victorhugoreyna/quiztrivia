09d3180b8cca4e199af4c96bc0753303c1b3f340
victorhugoreyna <68253953+victorhugoreyna@users.noreply.github.com>
2022-01-28 13:43:40 -0300

    format

--- user.js
+++ user.js
@@ -9,16 +9,19 @@ player = new User(0,0);
 function getScore() {
     return player.score;
 }
+
 function getQuestionN() {
     return player.questionN;
 }
-function updateScore(test){
+
+function updateScore(test){ //funcao para atualizar a pontuacao do jogador caso acertou a questao
     if(test==0){
       player.score = player.score+10;
       document.getElementById("score").innerHTML = player.score;
     }
 }
-function updateQuestionN(){
+
+function updateQuestionN(){ //atualizar o numero da questao atual
   player.questionN = player.questionN + 1;
   document.getElementById("number").innerHTML = player.questionN;
 
